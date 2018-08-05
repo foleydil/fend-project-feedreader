38bb4a640a448a17f651fc6558f94913b5cc0214
Dillon Foley <foleydil@gmail.com>
2018-08-05 13:52:32 -0400

    test: Test for menu in hidden state

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -44,7 +44,7 @@ $(function() {
          });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -62,19 +62,27 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
+    describe('The menu', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+      /* TODO: Write a test that ensures the menu element is
+      * hidden by default. You'll have to analyze the HTML and
+      * the CSS to determine how we're performing the
+      * hiding/showing of the menu element.
+      */
+      it('should be hidden by default', function() {
+        let body = document.getElementsByTagName('body')[0].classList;
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        console.log(body);
+        expect(body.contains('menu-hidden')).toBe(true);
+      })
+
+      /* TODO: Write a test that ensures the menu changes
+      * visibility when the menu icon is clicked. This test
+      * should have two expectations: does the menu display when
+      * clicked and does it hide when clicked again.
+      */
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
